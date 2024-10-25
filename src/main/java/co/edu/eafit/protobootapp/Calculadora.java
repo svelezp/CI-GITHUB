09a1101cc43b6abbbd2815021eecf8516413f784
svelezp <69641241+svelezp@users.noreply.github.com>
2024-10-25 18:38:37 -0500

    cambio test

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -8,7 +8,7 @@ public class Calculadora {
     }
     
     public Double cubo(Double numero) {
-        return numero * numero * numeronumero54325234;
+        return numero * numero * numero;
     }
 }
 
